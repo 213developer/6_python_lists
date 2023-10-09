075340251bb4d595956fb47a358a6088c93d41c1
“javascriptseo” <seofreelance2021@outlook.com>
2023-10-08 17:36:37 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Declare a named constant for array size here.
+MAX_AVERAGES = 8
+
+# Declare array here.
+
+# Write a loop to get batting averages from user and assign to array.
+
+    averageString = input("Enter a batting average: ")
+    battingAverage = float(averageString)
+    # Assign value to array.
+
+# Assign the first element in the array to be the minimum and the maximum.
+minAverage = averages[0]
+maxAverage = averages[0]
+# Start out your total with the value of the first element in the array.
+total = averages[0]
+# Write a loop here to access array values starting with averages[1]
+
+    # Within the loop test for minimum and maximum batting averages.
+
+
+
+
+    # Also accumulate a total of all batting averages.
+
+
+# Calculate the average of the 8 batting averages.
+
+
+# Print the batting averages stored in the averages array.
+
+
+# Print the maximum batting average, minimum batting average, and average batting average.
